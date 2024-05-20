5db8b9244d1a5ab77087364ab47fa44f62495585
Darius Baciu <baciu.darius01@gmail.com>
2024-05-20 10:20:48 +0300

    project setup and home page

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+import React, { Children } from 'react'
+import '@styles/globals.css';
+
+export const metadata = {
+    title: "Promptopia",
+    description: "Discover & Share AI Prompts"    
+}
+
+function RootLayout({children}) {
+  return (
+   <html lang="en">
+    <body>
+        <div className='main'>
+            <div className='gradient'></div>
+        </div>
+
+        <main className='app'>
+            {children}
+        </main>
+    </body>
+   </html>
+  )
+}
+
+export default RootLayout
