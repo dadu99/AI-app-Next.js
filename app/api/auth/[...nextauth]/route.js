e5b11756f43aa71e086294fade4c9dff3e437888
Darius Baciu <baciu.darius01@gmail.com>
2024-05-21 15:09:20 +0300

    https://www.mongodb.com/community/forums/t/nextjs-env-file-problem-mongooseerror-the-uri-parameter-to-openuri-must-be-a-string-got-undefined-make-sure-the-first-parameter-to-mongoose-connect-or-mongoose-createconnection-is-a-string/176772/3

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -3,6 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 
 import User from "@models/user";
 import { connectToDB } from "@utils/database";
+require("dotenv").config();
 
 const handler = NextAuth({
   providers: [
--- utils/database.js
+++ utils/database.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 let isConnected = false; // track the connection
+require("dotenv").config();
 
 export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
